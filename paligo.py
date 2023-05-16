66e7540afb239ce87c72bc2e9b99661fa82828b3
Marco Dorr <marcomdorr@gmail.com>
2023-05-16 09:20:29 +0200

    Removed download link

--- paligo.py
+++ paligo.py
@@ -83,12 +83,6 @@ def generate_notes_text(df, patch_version):
                 notes_text += f'{release_note}</p>'
     return notes_text
 
-# Function to download the text file
-def get_text_file_download_link(text, filename='data.txt', title='Download text file'):
-    """Generates a link to download the text file"""
-    b64 = base64.b64encode(text.encode()).decode()  # some strings <-> bytes conversions necessary here
-    return f'<a href="data:file/txt;base64,{b64}" download="{filename}">{title}</a>'
-
 
 # Streamlit code
 st.title("CSV to Text")
@@ -103,6 +97,4 @@ if uploaded_file is not None:
     # Display the generated text in a markdown component to support HTML formatting
     st.markdown(notes_text, unsafe_allow_html=True)
 
-    # Provide a link to download the generated text as a .txt file
-    st.markdown(get_text_file_download_link(notes_text), unsafe_allow_html=True)
 
